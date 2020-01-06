c837a12c6663e76732627833833f8f060c8e628f
Arunkumar Arjunan <arunkumar.a@prodapt.com>
2020-01-06 09:02:03 +0530

    add eureka client config

--- src/main/java/com/pro/app/PdnUsersApplication.java
+++ src/main/java/com/pro/app/PdnUsersApplication.java
@@ -4,12 +4,10 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-import com.pro.app.utils.LoadAllProperties;
-
+@EnableDiscoveryClient
 @SpringBootApplication
-@EnableConfigurationProperties(LoadAllProperties.class)
 public class PdnUsersApplication {
 	
 	private static final Logger log = LogManager.getLogger(PdnUsersApplication.class.getName());
